2ffc4d3329a2ea9f5f6ecdb5d5f7cab791070663
JohnCh-867 <johnandersonchaponanm@gmail.com>
2026-04-22 03:09:03 -0500

    Update ClienteRepository.java

--- src/main/java/com/utp/veterinaria/Repository/GUsuarios/ClienteRepository.java
+++ src/main/java/com/utp/veterinaria/Repository/GUsuarios/ClienteRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
-    @Query("SELECT new com.example.vet.DTO.ClienteDTO(" +
+    @Query("SELECT new com.utp.veterinaria.DTO.ClienteDTO(" +
             "c.id, c.dni, c.nombres, c.apellidos, c.direccion, c.telefono, c.correo, " +
             "size(c.mascotas)) " +
             "FROM Cliente c")
